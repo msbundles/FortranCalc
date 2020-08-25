dbbe0d6ea6e7e7bf41c42bab922bd206aaf40377
msbundles <25093511+msbundles@users.noreply.github.com>
2020-08-24 22:01:29 -0500

    I did some formatting.

--- main.F90
+++ main.F90
@@ -1,57 +1,57 @@
 program UltraCalc
-implicit none
-character :: op
-print *,'Type + for add'
-print *,'Type - for subtract'
-print *,'Type * for multiply'
-print *,'Type d for divide'
-print *,'What would you like to do?: '
-read *,op
-if (op == '+') then
-   call add()
-else if (op == '-') then
-   call sub()
-else if (op == '*') then
-   call mult()
-else if (op == 'd') then
-   call div()
-else
-   print *,'Your input was not one of the known symbols.'
-end if
+  implicit none
+  character :: op
+  print *,'Type + for add'
+  print *,'Type - for subtract'
+  print *,'Type * for multiply'
+  print *,'Type d for divide'
+  print *,'What would you like to do?: '
+  read *,op
+  if (op == '+') then
+     call add()
+  else if (op == '-') then
+     call sub()
+  else if (op == '*') then
+     call mult()
+  else if (op == 'd') then
+     call div()
+  else
+     print *,'Your input was not one of the known symbols.'
+  end if
 end program UltraCalc
 
 ! Addition function
 subroutine add()
-real :: in1,in2,out
-print *,'Input two integers or real numbers (separated by a return): '
-read *,in1,in2
-out = in1 + in2
-print *,out
+  real :: in1,in2,out
+  print *,'Input two integers or real numbers (separated by a return): '
+  read *,in1,in2
+  out = in1 + in2
+  print *,out
 end subroutine add
 
 ! Subtraction function
 subroutine sub()
-real :: in1,in2,out
-print *,'Input two integers or real numbers (separated by a return): '
-read *,in1,in2
-out = in1 - in2
-print *,out
+  real :: in1,in2,out
+  print *,'Input two integers or real numbers (separated by a return): '
+  read *,in1,in2
+  out = in1 - in2
+  print *,out
 end subroutine sub
 
 ! Multiplication function 
 subroutine mult()
-real :: in1,in2,out
-print *,'Input two integers or real numbers (separated by a return): '
-read *,in1,in2
-out = in1 * in2
-print *,out
+  real :: in1,in2,out
+  print *,'Input two integers or real numbers (separated by a return): '
+  read *,in1,in2
+  out = in1 * in2
+  print *,out
 end subroutine mult
 
 ! Division function
 subroutine div()
-real :: in1,in2,out
-print *,'Input two integers or real numbers (separated by a return): '
-read *,in1,in2
-out = in1 / in2
-print *,out
+  real :: in1,in2,out
+  print *,'Input two integers or real numbers (separated by a return): '
+  read *,in1,in2
+  out = in1 / in2
+  print *,out
 end subroutine div
